3944421bfa6702e17d6517ad7e226bcfc67c2c78
aab500 <92261855+aab500@users.noreply.github.com>
2024-08-23 16:01:32 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default {
-  // other config options
-  build: {
-    rollupOptions: {
-      external: ['/src/main.jsx']
-    }
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: '0.0.0.0',
   }
-};
+})
