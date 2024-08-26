7117a6834010d397f31cb332d7ca7b1d0c8ae8b6
aab500 <92261855+aab500@users.noreply.github.com>
2024-08-26 07:58:00 -0500

    Update issueConfig.js

--- src/data/issueConfig.js
+++ src/data/issueConfig.js
@@ -4,20 +4,28 @@ export const issueConfig = [
   { name: 'Motherboard', color: 'primary' },
   { name: 'Daughterboard', color: 'primary'},
   { name: 'Ports', color: 'primary' },
+  
   { name: 'LCD_NTS', color: 'primary' },
   { name: 'LCD_TouchScreen', color: 'primary' },
   { name: 'LCD_Cable', color: 'primary' },
+  { name: 'WebCam', color: 'primary' },
+
+  { name: 'Plastics', color: 'primary' },
+  { name: 'Hinges', color: 'primary' },
+
   { name: 'Keyboard', color: 'primary' },
   { name: 'Keyboard_WFC', color: 'primary' },
   { name: 'TouchPad', color: 'primary' },
-  { name: 'WebCam', color: 'primary' },
-  { name: 'Plastics', color: 'primary' },
-  { name: 'Hinges', color: 'primary' },
+
   { name: 'Battery', color: 'primary' },
-  { name: 'Liquid_Spill_Affected', color: 'primary' },
+  { name: 'Wifi', color: 'primary' },
+
   { name: 'OS', color: 'primary' },
+  { name: 'Sensors', color: 'primary' },
   { name: 'Adapter', color: 'primary' },
   { name: 'Stylus', color: 'primary' },
+
+  { name: 'Liquid_Spill_Affected', color: 'primary' },
   { name: 'NTF', color: 'danger' },
   { name: 'Wrong_Claim', color: 'danger' },
   { name: 'High_Repair_Cost_DEF', color: 'info' }    
