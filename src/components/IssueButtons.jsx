08569d4f4bcd13932f58cdc28d995543a0508873
aab500 <92261855+aab500@users.noreply.github.com>
2024-08-26 17:20:31 -0500

    Update IssueButtons.jsx

--- src/components/IssueButtons.jsx
+++ src/components/IssueButtons.jsx
@@ -6,14 +6,18 @@ import { issueConfig } from '../data/issueConfig';
 function IssueButtons({ setCurrentIssue }) {
   return (
     <div className="form-group">
-      {issueConfig.map(({ name, color }) => (
-        <button
-          key={name}
-          className={`btn btn-${color} m-1`}
-          onClick={() => setCurrentIssue(name)}
-        >
-          {name.replace(/_/g, ' ')}
-        </button>
+      {issueConfig.map((row, rowIndex) => (
+        <div key={rowIndex} className="d-flex flex-wrap mb-2">
+          {row.map(({ name, color }) => (
+            <button
+              key={name}
+              className={`btn btn-${color} m-1`}
+              onClick={() => setCurrentIssue(name)}
+            >
+              {name.replace(/_/g, ' ')}
+            </button>
+          ))}
+        </div>
       ))}
     </div>
   );
